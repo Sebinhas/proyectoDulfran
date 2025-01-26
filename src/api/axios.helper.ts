16500972f2428fb488d99e7bceea71d0612786ac
JuanesEspinosa <juanes.espinosa01@gmail.com>
2025-01-25 22:04:32 -0500

    masivo de usuarios

--- src/api/axios.helper.ts
+++ src/api/axios.helper.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://a453-186-84-88-166.ngrok-free.app/api";
+export const BASE_URL = "https://be1a-186-84-88-166.ngrok-free.app/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
